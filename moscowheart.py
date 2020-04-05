26fd276fd477796c0db9027f3a697ad58e5217ad
Marko Kevac <marko@kevac.org>
2020-04-05 23:15:57 +0300

    use selenium to get proper url with proper token

--- moscowheart.py
+++ moscowheart.py
@@ -3,18 +3,66 @@ import urllib.request
 import os
 import time
 
-URL = 'https://stream.exdesign.ru/serdcestolicy-1/index.m3u8?token=ee8d8f8029152285366c30abd7874b151dac9d87-3492a6bb71f678f1cd29652b9607214d-1586127742-1586116942'
-BASEURL = URL[0:URL.rindex('/')]
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def getURL():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.headless = True
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://moscowheart.ru")
+
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "open_webcam"))
+        )
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "open_webcam"))
+        )
+    except Exception:
+        return
+
+    # still need this :-(
+    time.sleep(2)
+
+    elem = driver.find_element_by_class_name("open_webcam")
+    elem.click()
+
+    # still need this :-(
+    time.sleep(2)
+
+    playeriframe = driver.find_element_by_tag_name("iframe")
+    driver.switch_to.frame(playeriframe)
+    for script in driver.find_elements_by_tag_name("script"):
+        innerHTML = str(script.get_attribute('innerHTML'))
+        try:
+            ind = innerHTML.rindex("m3u8") + 4
+            indEnd = ind + innerHTML[ind:].index("'")
+            indBegin = innerHTML[:ind].rindex("'")+1
+            result = "https:"+innerHTML[indBegin:indEnd]
+        except Exception:
+            pass
+
+    driver.close()
+    return result
 
 
 def main():
+    url = getURL()
+    baseurl = url[0:url.rindex('/')]
+
     while True:
-        m3u8_obj = m3u8.load(URL)  # this could also be an absolute filename
+        m3u8_obj = m3u8.load(url)  # this could also be an absolute filename
         playlist = m3u8_obj.playlists[0]
         m3u8_obj = m3u8.load(playlist.absolute_uri)
-        lastfileURL = BASEURL+'/tracks-v1/'+m3u8_obj.files[-1]
-        localFile, _ = urllib.request.urlretrieve(lastfileURL)
-        os.system("ffmpeg -i {} -r 1 -f singlejpeg {}".format(localFile, 'data/'+time.strftime("%F-%H-%M-%S")+'.jpg'))
+        lastfile_url = baseurl + '/tracks-v1/' + m3u8_obj.files[-1]
+        local_file, _ = urllib.request.urlretrieve(lastfile_url)
+        os.system(
+            "ffmpeg -i {} -r 1 -f singlejpeg {}".format(local_file, 'data/' + time.strftime("%F-%H-%M-%S") + '.jpg'))
+        os.remove(local_file)
         time.sleep(60)
 
 
